31ba67453d1ef8b9630845c0e204ef4fcaacf5f7
Hargun22 <hargunbedi463@gmail.com>
2022-11-28 20:15:35 -0500

    added boilerplate for transumter

---
+++ src/gameEngine/transumter.py
@@ -0,0 +1,27 @@
+class Transmuter:
+    def __init__(self):
+        pass
+
+    # gets all the tiles currently on the transmuter
+    def getAllTiles(self):
+        pass
+
+    # returns the tile at the given position
+    def getTile(self, position):
+        pass
+
+    # adds tile type to the front position
+    def addTileToFront(self, tile):
+        pass
+
+    # remove last tile
+    def removeTileFromEnd(self):
+        pass
+
+    # helper func to move the tiles after removing a tile
+    def _moveAllTilesAhead(self):
+        pass
+
+    def isFull(self):
+        pass
+    
