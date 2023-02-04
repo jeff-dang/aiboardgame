8b6dc584003db05b68b78e6be96ff2adfbac2d00
enganddeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2023-02-04 15:08:33 -0500

    mid feature push to avoid conflicts

--- src/AnAgeContrived/env/entities/action_tokens.py
+++ src/AnAgeContrived/env/entities/action_tokens.py
@@ -1,7 +1,25 @@
 
-class ActionToken():
+class EnergyActionToken():
+
+    def __init__(self, transmuter_tiles: list, times: int) -> None:
+        self.transmuter_tiles = transmuter_tiles #for energy release action_token, which transmuter tiles can release energy?
+        self.times = times #how many times can it be used, you can release two vs 1 energy, you can move 1 or 2 times on the board etc.
+
+class MoveActionToken():
+
+    def __init__(self, move_times: int, transformative_times: int, sentinent_times: int) -> None:
+        self.move_time = move_times
+        self.transformative_times = transformative_times
+        self.sentinent_times = sentinent_times
+
+#TODO: just pass this for now and implement if time left
+class RechargeActionToken():
 
     def __init__(self) -> None:
-        self.action_token_type = None
-        self.transmuter_tiles = [0, 0, 0, 1, 1] #for energy release action_token, which transmuter tiles can release energy?
-        self.times = 1 #how many times can it be used, you can release two vs 1 energy, you can move 1 or 2 times on the board etc.
+        pass
+
+#TODO: just pass this for now and implement if time left
+class RepositionActionToken():
+
+    def __init__(self) -> None:
+        pass
