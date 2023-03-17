9b019296109fa2f9b16e02e7e1aa01e065e308f2
michaelilao <michaelilao@live.com>
2023-03-16 20:28:13 -0400

    visualiuzer

--- src/visualization_v1/src/data/getFiles.js
+++ src/visualization_v1/src/data/getFiles.js
@@ -1,7 +1,5 @@
 export const files = [
-  "../files/game.json",
-  "../files/game2.json",
-  "../files/simulation_history_03_13_2023_16_32_09.json",
-  "../files/simulation_history_03_13_2023_16_27_42.json",
-  "../files/simulation_history_03_13_2023_19_48_18.json",
+  {"name":"sim1", "filename": "../files/simulation_history_03_16_2023_20_00_32"},
+  {"name":"sim2", "filename": "../files/game2.json"}
+
 ];
