810b7de1af8a1dca2ed912742790214286a7dd79
enganddeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2023-03-20 00:34:11 -0400

    Latest stable version with new tracks and poc features implemented

--- src/AnAgeContrived/env/env.py
+++ src/AnAgeContrived/env/env.py
@@ -31,7 +31,7 @@ class raw_env(AECEnv):
 
     def __init__(self, render_mode=None):
         super().__init__()
-        self.output_json = True
+        self.output_json = False
         self.engine = Engine()
         self.agents = self.engine.get_agents()
         self.possible_agents = self.agents[:]
